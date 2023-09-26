828271f6e2398fabd047d458b5b06655e0e1757d
Lucas Falco <lucasfalco7318@gmail.com>
2023-09-25 22:47:46 -0300

    desafio 2 - cards autogeneradas

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CardsComponent {
 
+  cardVec: string [] = ["../../assets/images/Fallout-4.png","../../assets/images/Detroit-Become-Human.jpg","../../assets/images/horizon.jpg","../../assets/images/skyrim.jpg","../../assets/images/Cyberpunk.jpg" ];
+  
+  agregarCard(){
+    console.log(this.cardVec[0])
+    return this.cardVec[0]
+  }
 }
